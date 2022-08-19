98a33e6586874ff88f4effc7a13d00c4552876c7
Lucas Antonsen <ljantonsen@hotmail.ca>
2022-08-18 21:49:32 -0700

    Added brief comments to qr.c.

--- qr.c
+++ qr.c
@@ -8,13 +8,13 @@
 
 
 int main(int argc, char *argv[]) {
-	clock_t start, finish;
+	clock_t start, finish;			//for measuring program runtime
 
 	char *end;
 	char delim[] = " ";	
-	assert(argc == 5);
-	char *fin = argv[1];
-	char *fout = argv[2];
+	assert(argc == 5);		//./qr.exe input_m.txt output_m.txt ROWS COLS
+	char *fin = argv[1];	//file input pointer for matrix input
+	char *fout = argv[2];	//file output pointer for QR output
 	SIZE_T ROWS = (SIZE_T) strtoull(argv[3], &end, 10);
 	SIZE_T COLS = (SIZE_T) strtoull(argv[4], &end, 10);
 	
@@ -26,29 +26,29 @@ int main(int argc, char *argv[]) {
 	SIZE_T i;
 	double time_taken = 0;
 
-	for(i = 0; i < 100; i++){
-		start = clock();
+	for(i = 0; i < 100; i++){	//run 100 times
+		start = clock();		//start time
 
-		zero_m(ROWS, COLS, A);
+		zero_m(ROWS, COLS, A);	//zero out matrices so garbage values aren't used in calculations
 		zero_m(COLS, ROWS, At);
 		zero_m(ROWS, ROWS, Q);
 		zero_m(ROWS, COLS, R);
 		
-		fscanm(fin, delim, ROWS, COLS, A);	
-		transpose_m(ROWS, COLS, A, At, 0, ROWS, 0, COLS);
-		//transpose_m(ROWS, COLS, A, At);
+		fscanm(fin, delim, ROWS, COLS, A);					//load input matrix values into matrix A
+		transpose_m(ROWS, COLS, A, At, 0, ROWS, 0, COLS);	//new cache-oblivious matrix transpose function call
+		//transpose_m(ROWS, COLS, A, At);					//old matrix transpose function call
 		
-		QR(ROWS, COLS, At, Q, R);
+		QR(ROWS, COLS, At, Q, R);				//call QR to perform the QR decomposition
 		
-		fprintmt(fout, delim, ROWS, ROWS, Q);
-		fprintmt(fout, delim, ROWS, COLS, R);	
+		fprintmt(fout, delim, ROWS, ROWS, Q);	//print Q to output file
+		fprintmt(fout, delim, ROWS, COLS, R);	//print R to output file
 
-		finish = clock();
+		finish = clock();	//end time
 	
-		// Calculating total time taken by the program.
-		time_taken += ((float) (finish - start)) / CLOCKS_PER_SEC;
+		//calculating total time taken by the program
+		time_taken += ((float) (finish - start)) / CLOCKS_PER_SEC;	//add measurement for this run to the total time taken
 	}
-	printf ("Total time = %lf seconds\n", time_taken/100.0);
+	printf ("Total time = %lf seconds\n", time_taken/100.0);	//divide the total time taken for all runs by 100 to get the average
 
 	return 0;
 }
